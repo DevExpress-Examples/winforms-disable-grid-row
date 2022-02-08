2d8668af55166aede91fa73653c89cbf9f5e8299
DevExpressExampleBot <fakeEmail>
2022-02-08 15:34:28 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -1,105 +1,106 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.rowStateHelper1 = New WindowsApplication1.RowStateHelper()
-			Me.propertyGrid1 = New System.Windows.Forms.PropertyGrid()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(501, 588)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			' 
-			' gridView2
-			' 
-			Me.gridView2.GridControl = Me.gridControl1
-			Me.gridView2.Name = "gridView2"
-			' 
-			' rowStateHelper1
-			' 
-			Me.rowStateHelper1.AppearanceDisabledRow.BackColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(224))))), (CInt(Fix((CByte(224))))), (CInt(Fix((CByte(224))))))
-			Me.rowStateHelper1.AppearanceDisabledRow.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, (CByte(204)))
-			Me.rowStateHelper1.AppearanceDisabledRow.ForeColor = System.Drawing.Color.Gray
-			Me.rowStateHelper1.AppearanceDisabledRow.Options.UseBackColor = True
-			Me.rowStateHelper1.AppearanceDisabledRow.Options.UseFont = True
-			Me.rowStateHelper1.AppearanceDisabledRow.Options.UseForeColor = True
-			Me.rowStateHelper1.DisabledRows.Add(0)
-			Me.rowStateHelper1.DisabledRows.Add(3)
-			Me.rowStateHelper1.DisabledRows.Add(9)
-			Me.rowStateHelper1.GridView = Me.gridView1
-			' 
-			' propertyGrid1
-			' 
-			Me.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Right
-			Me.propertyGrid1.Location = New System.Drawing.Point(501, 0)
-			Me.propertyGrid1.Name = "propertyGrid1"
-			Me.propertyGrid1.SelectedObject = Me.rowStateHelper1
-			Me.propertyGrid1.Size = New System.Drawing.Size(244, 588)
-			Me.propertyGrid1.TabIndex = 1
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(745, 588)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Controls.Add(Me.propertyGrid1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.rowStateHelper1 = New WindowsApplication1.RowStateHelper()
+            Me.propertyGrid1 = New System.Windows.Forms.PropertyGrid()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView2), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(501, 588)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1, Me.gridView2})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' gridView2
+            ' 
+            Me.gridView2.GridControl = Me.gridControl1
+            Me.gridView2.Name = "gridView2"
+            ' 
+            ' rowStateHelper1
+            ' 
+            Me.rowStateHelper1.AppearanceDisabledRow.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((224)))))), (CInt(((CByte((224)))))), (CInt(((CByte((224)))))))
+            Me.rowStateHelper1.AppearanceDisabledRow.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, (CByte((204))))
+            Me.rowStateHelper1.AppearanceDisabledRow.ForeColor = System.Drawing.Color.Gray
+            Me.rowStateHelper1.AppearanceDisabledRow.Options.UseBackColor = True
+            Me.rowStateHelper1.AppearanceDisabledRow.Options.UseFont = True
+            Me.rowStateHelper1.AppearanceDisabledRow.Options.UseForeColor = True
+            Me.rowStateHelper1.DisabledRows.Add(0)
+            Me.rowStateHelper1.DisabledRows.Add(3)
+            Me.rowStateHelper1.DisabledRows.Add(9)
+            Me.rowStateHelper1.GridView = Me.gridView1
+            ' 
+            ' propertyGrid1
+            ' 
+            Me.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.propertyGrid1.Location = New System.Drawing.Point(501, 0)
+            Me.propertyGrid1.Name = "propertyGrid1"
+            Me.propertyGrid1.SelectedObject = Me.rowStateHelper1
+            Me.propertyGrid1.Size = New System.Drawing.Size(244, 588)
+            Me.propertyGrid1.TabIndex = 1
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(745, 588)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Controls.Add(Me.propertyGrid1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView2), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private rowStateHelper1 As RowStateHelper
-		Private propertyGrid1 As System.Windows.Forms.PropertyGrid
-	End Class
+#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private rowStateHelper1 As WindowsApplication1.RowStateHelper
+
+        Private propertyGrid1 As System.Windows.Forms.PropertyGrid
+    End Class
 End Namespace
-
--- VB/WindowsApplication1/RowStateHelper.vb
+++ VB/WindowsApplication1/RowStateHelper.vb
@@ -1,100 +1,91 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
-Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.Utils
 
 Namespace WindowsApplication1
-	Public Class RowStateHelper
-		Inherits Component
-		Private _AppearanceDisabledRow As AppearanceObject
 
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
-		Public ReadOnly Property AppearanceDisabledRow() As AppearanceObject
-			Get
-				Return _AppearanceDisabledRow
-			End Get
-		End Property
+    Public Class RowStateHelper
+        Inherits Component
 
-		Private _GridView As GridView
-		Public Property GridView() As GridView
-			Get
-				Return _GridView
-			End Get
-			Set(ByVal value As GridView)
-				UnSubscribeEvents(value)
-				_GridView = value
-				SubscribeEvents(value)
-			End Set
-		End Property
+        Private _AppearanceDisabledRow As AppearanceObject
 
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)>
+        Public ReadOnly Property AppearanceDisabledRow As AppearanceObject
+            Get
+                Return _AppearanceDisabledRow
+            End Get
+        End Property
 
-		Private _DisabledRows As List(Of Integer)
+        Private _GridView As GridView
 
-		<DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
-		Public ReadOnly Property DisabledRows() As List(Of Integer)
-			Get
-				Return _DisabledRows
-			End Get
-		End Property
+        Public Property GridView As GridView
+            Get
+                Return _GridView
+            End Get
 
-		Public Sub New()
-			_DisabledRows = New List(Of Integer)()
-			_AppearanceDisabledRow = New AppearanceObject()
-			AddHandler AppearanceDisabledRow.Changed, AddressOf _AppearanceDisabledRow_Changed
-		End Sub
+            Set(ByVal value As GridView)
+                UnSubscribeEvents(value)
+                _GridView = value
+                SubscribeEvents(value)
+            End Set
+        End Property
 
-		Private Sub _AppearanceDisabledRow_Changed(ByVal sender As Object, ByVal e As EventArgs)
-			If GridView Is Nothing Then
-				Return
-			End If
-			For i As Integer = 0 To DisabledRows.Count - 1
-				GridView.RefreshRow(GridView.GetRowHandle(DisabledRows(i)))
-			Next i
+        Private _DisabledRows As List(Of Integer)
 
-		End Sub
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)>
+        Public ReadOnly Property DisabledRows As List(Of Integer)
+            Get
+                Return _DisabledRows
+            End Get
+        End Property
 
-		Private Sub UnSubscribeEvents(ByVal view As GridView)
-			RemoveHandler view.RowCellStyle, AddressOf view_RowCellStyle
-			RemoveHandler view.ShowingEditor, AddressOf view_ShowingEditor
+        Public Sub New()
+            _DisabledRows = New List(Of Integer)()
+            _AppearanceDisabledRow = New AppearanceObject()
+            AddHandler AppearanceDisabledRow.Changed, AddressOf _AppearanceDisabledRow_Changed
+        End Sub
 
-		End Sub
-		Private Sub SubscribeEvents(ByVal view As GridView)
-			AddHandler view.RowCellStyle, AddressOf view_RowCellStyle
-			AddHandler view.ShowingEditor, AddressOf view_ShowingEditor
-		End Sub
+        Private Sub _AppearanceDisabledRow_Changed(ByVal sender As Object, ByVal e As EventArgs)
+            If GridView Is Nothing Then Return
+            For i As Integer = 0 To DisabledRows.Count - 1
+                GridView.RefreshRow(GridView.GetRowHandle(DisabledRows(i)))
+            Next
+        End Sub
 
-		Public Sub DisableRow(ByVal dataSourceRowIndex As Integer)
-			DisabledRows.Add(dataSourceRowIndex)
-		End Sub
+        Private Sub UnSubscribeEvents(ByVal view As GridView)
+            RemoveHandler view.RowCellStyle, AddressOf view_RowCellStyle
+            RemoveHandler view.ShowingEditor, AddressOf view_ShowingEditor
+        End Sub
 
+        Private Sub SubscribeEvents(ByVal view As GridView)
+            AddHandler view.RowCellStyle, AddressOf view_RowCellStyle
+            AddHandler view.ShowingEditor, AddressOf view_ShowingEditor
+        End Sub
 
-		Public Function IsRowDisabled(ByVal dataSourceRowIndex As Integer) As Boolean
-			Return DisabledRows.Contains(dataSourceRowIndex)
-		End Function
+        Public Sub DisableRow(ByVal dataSourceRowIndex As Integer)
+            DisabledRows.Add(dataSourceRowIndex)
+        End Sub
 
-		Public Sub EnableRow(ByVal dataSourceRowIndex As Integer)
-			Do While IsRowDisabled(dataSourceRowIndex)
-				DisabledRows.Remove(dataSourceRowIndex)
-			Loop
-		End Sub
+        Public Function IsRowDisabled(ByVal dataSourceRowIndex As Integer) As Boolean
+            Return DisabledRows.Contains(dataSourceRowIndex)
+        End Function
 
+        Public Sub EnableRow(ByVal dataSourceRowIndex As Integer)
+            While IsRowDisabled(dataSourceRowIndex)
+                DisabledRows.Remove(dataSourceRowIndex)
+            End While
+        End Sub
 
-		Private Sub view_ShowingEditor(ByVal sender As Object, ByVal e As CancelEventArgs)
-			e.Cancel = IsRowDisabled(GridView.GetDataSourceRowIndex(GridView.FocusedRowHandle))
-		End Sub
+        Private Sub view_ShowingEditor(ByVal sender As Object, ByVal e As CancelEventArgs)
+            e.Cancel = IsRowDisabled(GridView.GetDataSourceRowIndex(GridView.FocusedRowHandle))
+        End Sub
 
-		Private Sub view_RowCellStyle(ByVal sender As Object, ByVal e As RowCellStyleEventArgs)
-			Console.WriteLine(DateTime.Now.Millisecond)
-			If IsRowDisabled(GridView.GetDataSourceRowIndex(e.RowHandle)) Then
-				e.Appearance.Assign(AppearanceDisabledRow)
-			End If
-		End Sub
-
-	End Class
+        Private Sub view_RowCellStyle(ByVal sender As Object, ByVal e As RowCellStyleEventArgs)
+            Console.WriteLine(Date.Now.Millisecond)
+            If IsRowDisabled(GridView.GetDataSourceRowIndex(e.RowHandle)) Then e.Appearance.Assign(AppearanceDisabledRow)
+        End Sub
+    End Class
 End Namespace
